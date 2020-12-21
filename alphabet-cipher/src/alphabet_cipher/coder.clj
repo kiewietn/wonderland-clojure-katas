194b2f71257c1ab0738c17673810416b1b6af602
Nico Kiewiet <ngkiewiet@gmail.com>
2020-12-21 17:11:34 +0200

    Alphabet cipher kata solved + decipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,34 @@
 (ns alphabet-cipher.coder)
 
+(def sub-chart (reduce (fn [acc c]
+                         (conj acc (mapv #(char (+ (mod % 26) 97)) (range c (+ c 26)))))
+                       []
+                       (range 26)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str
+         (map (fn [s k]
+                (let [index-s (- (int s) 97)
+                      index-k (- (int k) 97)]
+                  (nth (nth sub-chart index-s) index-k))) message (cycle keyword))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str
+         (map (fn [c k]
+                (let [index-k (- (int k) 97)]
+                  (loop [index 0
+                         row (nth sub-chart index-k)]
+                    (if (or (empty? row) (= c (first row)))
+                      (char (+ index 97))
+                      (recur (inc index) (rest row))))))
+              message
+              (cycle keyword))))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (let [key-candidates (decode message cipher)]
+    (reduce (fn [code nc]
+              (if (= message (decode code cipher))
+                (reduced code)
+                (str code nc)))
+            ""
+            key-candidates)))
